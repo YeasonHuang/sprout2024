112169b161b46b60a93550a83c2debf44dcbe9da
YeasonHuang <eason1qaz@gmail.com>
2024-03-02 16:42:29 +0800

    fine running of cin and cout

---
+++ dump/test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+signed main() {
+
+    string s;
+    cin >> s;
+    cout << s << "\n";
+
+    return 0;
+}
