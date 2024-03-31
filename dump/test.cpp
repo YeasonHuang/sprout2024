12c62cb14c6faa0f9102fa698127f3610929026c
YeasonHuang <eason1qaz@gmail.com>
2024-03-31 20:31:17 +0800

    done some problems

--- dump/test.cpp
+++ dump/test.cpp
@@ -1,72 +1,110 @@
-#pragma GCC optimize("Ofast") 
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma") 
-#pragma GCC optimize("unroll-loops") 
-#include <bits/stdc++.h>   
-#include <bits/extc++.h>
-#include <complex> 
-#include <queue> 
-#include <set> 
-#include <unordered_set> 
-#include <list> 
-#include <chrono> 
-#include <random> 
-#include <iostream> 
-#include <algorithm> 
-#include <cmath> 
-#include <string> 
-#include <vector> 
-#include <map> 
-#include <unordered_map> 
-#include <stack> 
-#include <iomanip> 
-#include <fstream> 
-   
-using namespace __gnu_pbds;
-using namespace std; 
-   
-typedef long long ll; 
-typedef long double ld; 
-typedef pair<int,int> p32; 
-typedef pair<ll,ll> p64; 
-typedef pair<double,double> pdd; 
-typedef vector<ll> v64; 
-typedef vector<int> v32; 
-typedef vector<vector<int> > vv32; 
-typedef vector<vector<ll> > vv64; 
-typedef vector<vector<p64> > vvp64; 
-typedef vector<p64> vp64; 
-typedef vector<p32> vp32; 
-ll MOD = 998244353; 
-double eps = 1e-12; 
-#define forn(i,e) for(ll i = 0; i < e; i++) 
-#define forsn(i,s,e) for(ll i = s; i < e; i++) 
-#define rforn(i,s) for(ll i = s; i >= 0; i--) 
-#define rforsn(i,s,e) for(ll i = s; i >= e; i--) 
-#define ln "\n" 
-#define dbg(x) cout<<#x<<" = "<<x<<ln 
-#define mp make_pair 
-#define pb push_back 
-#define fi first 
-#define se second 
-#define INF 2e18 
-#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
-#define all(x) (x).begin(), (x).end() 
-#define sz(x) ((ll)(x).size()) 
-   
-  
-void solve(){ 
-    tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> tr;
-    tr.insert(4),tr.insert(10),tr.insert(5);
-    cout << *tr.find_by_order(-1);
-} 
-int main() 
-{ 
-    fast_cin(); 
-    //ll t; 
-    //cin >> t; 
-    //for(int it=1;it<=t;it++) { 
-        //cout << "Case #" << it+1 << ": "; 
-        solve(); 
-    //} 
-    return 0; 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Sudoku{
+private:
+    int maze[9][9];
+    bool flag = false;
+public:
+    void print(string s);//
+    void scan_maze();//
+    vector<int> select(int, int);
+    int next_empty(int, int);
+    void solving();
+    void dfs(int row, int col );
+    
+};
+void Sudoku::print(string s){
+    cout<<endl<<"====="<<s<<"======"<<endl;
+    for (int i=0; i<9;i++){
+        cout<<"|";
+        for (int j=0;j<9;j++){
+            cout<<maze[i][j];
+            if(j%3==2)cout<<"|";
+        }
+        cout<<endl;
+        if(i%3==2)cout<<"-------------"<<endl;
+    }
+}
+vector<int> Sudoku::select(int r, int c){
+    bool box[9]={0};
+    for(int i=0;i<9;i++){
+        if(maze[i][c]>0)box[maze[i][c]-1] = 1;
+        if(maze[r][i]>0)box[maze[r][i]-1] = 1;
+    }
+    int row_start = 3*(r/3),col_start = 3*(c/3);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(maze[row_start+i][col_start+j]>0)
+                box[maze[row_start+i][col_start+j]-1] = 1;
+        }
+    }
+    vector<int> ans;
+    for(int i=0;i<9;i++)
+        if(!box[i])ans.push_back(i+1);
+    return ans;
+}
+int Sudoku::next_empty(int row, int col){
+    int ind = col;
+    for(int i=row;i<9;i++){
+        while(ind<9){
+            if(maze[i][ind]==0){
+                int pos = i*9+ind;
+                return pos;
+            }
+            ind++;
+        }
+        ind = 0;
+    }
+    return -1;
+}
+void Sudoku::dfs(int row, int col ){
+    int pos = next_empty(row, col),nr,nc;
+    if(pos == -1){
+        flag = true;
+        return;
+    }
+    vector<int> candidate = select(row, col);
+    int len = candidate.size();
+    
+    for(int i=0;i<len;i++){
+        maze[row][col] = candidate[i];
+        pos = next_empty(row, col);
+        nr = pos/9; nc = pos%9;
+        dfs(nr, nc);
+        if(flag)return;
+    }
+    maze[row][col] = 0;
+}
+void Sudoku::scan_maze(){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            char temp;cin>>temp;
+            if(temp=='.')maze[i][j] = 0;
+            else maze[i][j] = temp -'0';
+        }
+    }
+}
+void Sudoku::solving(){
+    int first_empty = next_empty(0, 0),nr,nc;
+    nr = first_empty/9; nc = first_empty%9;
+    dfs(nr, nc);
+    bool f = false;
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            vector<int> v = select(i,j);
+            if(v.size()>0)f = true;
+        }
+    }
+    if(!flag || f)cout<<"No solution."<<endl;
+    else{
+        print("solved");
+    }
+    flag = false;
+}
+int main(){
+    Sudoku sodoku1;
+    sodoku1.scan_maze();
+    //sodoku1.print("begin");
+    sodoku1.solving();
 }
